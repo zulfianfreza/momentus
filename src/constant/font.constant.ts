b3ef20faef8aa4fa3319b50f7a6fddc45318fb82
Zulfian Fachru Reza <julianfreza04@gmail.com>
2024-08-30 17:59:46 +0700

    fix: change google fonts to local fonts

--- src/constant/font.constant.ts
+++ src/constant/font.constant.ts
@@ -1,78 +1,314 @@
-import {
-  Abhaya_Libre,
-  Comfortaa,
-  Inika,
-  Inknut_Antiqua,
-  Inter,
-  Italiana,
-  Italianno,
-  Josefin_Sans,
-  Jost,
-  Judson,
-  Kumar_One,
-  Meow_Script,
-  Montserrat,
-  Notable,
-  Overlock,
-  Plus_Jakarta_Sans,
-  Poppins,
-  Roboto,
-  Rubik,
-  The_Nautigal,
-  Work_Sans,
-} from 'next/font/google';
 import localFont from 'next/font/local';
 
 // LOCAL FONT
+
+export const abhayaLibre = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Abhaya_Libre/AbhayaLibre-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Abhaya_Libre/AbhayaLibre-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Abhaya_Libre/AbhayaLibre-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Abhaya_Libre/AbhayaLibre-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Abhaya_Libre/AbhayaLibre-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
+});
+
+export const comfortaa = localFont({
+  src: '../../public/fonts/Comfortaa/Comfortaa-VariableFont_wght.ttf',
+});
+export const inika = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Inika/Inika-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Inika/Inika-Bold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+});
+export const inknutAntiqua = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Inknut_Antiqua/InknutAntiqua-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Inknut_Antiqua/InknutAntiqua-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Inknut_Antiqua/InknutAntiqua-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Inknut_Antiqua/InknutAntiqua-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Inknut_Antiqua/InknutAntiqua-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Inknut_Antiqua/InknutAntiqua-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Inknut_Antiqua/InknutAntiqua-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+});
+export const inter = localFont({
+  src: '../../public/fonts/Inter/Inter-VariableFont_opsz,wght.ttf',
+});
+export const italiana = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Italiana/Italiana-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+});
+export const italianno = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Italianno/Italianno-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+});
 export const javassoul = localFont({
   src: '../../public/fonts/javassoul/Javassoul.ttf',
+});
+export const josefinSans = localFont({
+  src: '../../public/fonts/Josefin_Sans/JosefinSans-VariableFont_wght.ttf',
+});
+export const jost = localFont({
+  src: '../../public/fonts/Jost/Jost-VariableFont_wght.ttf',
+});
+export const judson = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Judson/Judson-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Judson/Judson-Bold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+});
+export const kumarOne = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Kumar_One/KumarOne-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+});
+export const meowScript = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Meow_Script/MeowScript-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+});
+export const montserrat = localFont({
+  src: '../../public/fonts/Montserrat/Montserrat-VariableFont_wght.ttf',
+});
+export const notable = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Notable/Notable-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
 });
 export const otomanopeeOne = localFont({
   src: '../../public/fonts/otomanopee-one/otomanopee-one.woff2',
 });
-export const italianno = Italianno({ subsets: ['latin'], weight: ['400'] });
-export const italiana = Italiana({ subsets: ['latin'], weight: ['400'] });
-export const theNautigal = The_Nautigal({
-  subsets: ['latin'],
-  weight: ['400', '700'],
+export const overlock = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Overlock/Overlock-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Overlock/Overlock-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Overlock/Overlock-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
 });
-export const abhayaLibre = Abhaya_Libre({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800'],
+export const plusJakartaSans = localFont({
+  src: '../../public/fonts/Plus_Jakarta_Sans/PlusJakartaSans-VariableFont_wght.ttf',
 });
-export const meowScript = Meow_Script({ subsets: ['latin'], weight: ['400'] });
-export const notable = Notable({ subsets: ['latin'], weight: ['400'] });
-export const kumarOne = Kumar_One({ subsets: ['latin'], weight: ['400'] });
+export const poppins = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/Poppins-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+});
+export const roboto = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Roboto/Roboto-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Roboto/Roboto-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Roboto/Roboto-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Roboto/Roboto-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+});
+export const rubik = localFont({
+  src: '../../public/fonts/Rubik/Rubik-VariableFont_wght.ttf',
+});
+export const theNautigal = localFont({
+  src: [
+    {
+      path: '../../public/fonts/The_Nautigal/TheNautigal-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/The_Nautigal/TheNautigal-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+});
+export const workSans = localFont({
+  src: '../../public/fonts/Work_Sans/WorkSans-VariableFont_wght.ttf',
+});
 
-// font text
-export const overlock = Overlock({
-  subsets: ['latin'],
-  weight: ['400', '700', '900'],
-});
-export const rubik = Rubik({ subsets: ['arabic'] });
-export const comfortaa = Comfortaa({
-  subsets: ['latin'],
-});
-export const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['400', '500', '700', '900'],
-});
-export const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800', '900'],
-});
-export const josefinSans = Josefin_Sans({ subsets: ['latin'] });
-export const inter = Inter({ subsets: ['latin'] });
-export const jost = Jost({ subsets: ['latin'] });
-export const judson = Judson({ subsets: ['latin'], weight: ['400', '700'] });
-export const montserrat = Montserrat({ subsets: ['latin'] });
-export const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] });
-export const inika = Inika({ subsets: ['latin'], weight: ['400', '700'] });
-export const workSans = Work_Sans({ subsets: ['latin'] });
-export const inknutAntiqua = Inknut_Antiqua({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800', '900'],
-});
+// export const italianno = Italianno({ subsets: ['latin'], weight: ['400'] });
+// export const italiana = Italiana({ subsets: ['latin'], weight: ['400'] });
+// export const theNautigal = The_Nautigal({
+//   subsets: ['latin'],
+//   weight: ['400', '700'],
+// });
+// export const abhayaLibre = Abhaya_Libre({
+//   subsets: ['latin'],
+//   weight: ['400', '500', '600', '700', '800'],
+// });
+// export const meowScript = Meow_Script({ subsets: ['latin'], weight: ['400'] });
+// export const notable = Notable({ subsets: ['latin'], weight: ['400'] });
+// export const kumarOne = Kumar_One({ subsets: ['latin'], weight: ['400'] });
+
+// // font text
+// export const overlock = Overlock({
+//   subsets: ['latin'],
+//   weight: ['400', '700', '900'],
+// });
+// export const rubik = Rubik({ subsets: ['arabic'] });
+// export const comfortaa = Comfortaa({
+//   subsets: ['latin'],
+// });
+// export const roboto = Roboto({
+//   subsets: ['latin'],
+//   weight: ['400', '500', '700', '900'],
+// });
+// export const poppins = Poppins({
+//   subsets: ['latin'],
+//   weight: ['400', '500', '600', '700', '800', '900'],
+// });
+// export const josefinSans = Josefin_Sans({ subsets: ['latin'] });
+// export const inter = Inter({ subsets: ['latin'] });
+// export const jost = Jost({ subsets: ['latin'] });
+// export const judson = Judson({ subsets: ['latin'], weight: ['400', '700'] });
+// export const montserrat = Montserrat({ subsets: ['latin'] });
+// export const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] });
+// export const inika = Inika({ subsets: ['latin'], weight: ['400', '700'] });
+// export const workSans = Work_Sans({ subsets: ['latin'] });
+// export const inknutAntiqua = Inknut_Antiqua({
+//   subsets: ['latin'],
+//   weight: ['400', '500', '600', '700', '800', '900'],
+// });
 
 export const LIST_FONT_TITLE = [
   {
