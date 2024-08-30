1be6f338f3c52fb8e0fc75789aa878d157e0f3b1
Zulfian Fachru Reza <julianfreza04@gmail.com>
2024-08-30 20:14:56 +0700

    fix: add bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
+import withBundleAnalyzer from '@next/bundle-analyzer';
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "**" },
-      { protocol: "http", hostname: "**" },
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: '**' },
     ],
   },
 };
 
-export default nextConfig;
+/** @type {import('@next/bundle-analyzer')} */
+const analyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+export default analyzer(nextConfig);
